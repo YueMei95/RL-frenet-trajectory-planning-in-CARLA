677ff7026fb05d0a96e995b84a077f08a52bd101
MajidMoghadam2006 <majid.moghadam2006@gmail.com>
2020-02-20 18:21:09 -0800

    separate control module from module manager

--- carla_gym/envs/carla_env.py
+++ carla_gym/envs/carla_env.py
@@ -39,17 +39,9 @@ class CarlaGymEnv(gym.Env):
 
     def step(self, action=0):
         self.module_manager.tick()  # Update carla world and lat/lon controllers
+        self.control_module.tick()
         reward = np.array([0.0])
         done = np.array([False])
-        # ego_transform = self.world_module.hero_actor.get_transform()
-
-        # psi = math.radians(ego_transform.rotation.yaw)
-
-        # xwp, ywp = 5, 5
-        # wpi = self.world_module.body_to_inertial_frame(xwp, ywp, psi)
-        # self.world_module.points_to_draw['testWP'] = carla.Location(x=wpi[0], y=wpi[1])
-        # print('x= {0:0.6f},  y= {1:0.6f}'.format(x, y))
-
         self.state = np.array([0, 0])
         return self.state, reward, done, {}
 
@@ -72,10 +64,12 @@ class CarlaGymEnv(gym.Env):
             self.input_module = ModuleInput(MODULE_INPUT, module_manager=self.module_manager)
             self.module_manager.register_module(self.input_module)
         self.control_module = ModuleControl(MODULE_CONTROL, module_manager=self.module_manager)
-        self.module_manager.register_module(self.control_module)
+        # We do not register control module bc we want to tick control separately
+        # self.module_manager.register_module(self.control_module)
 
         # Start Modules
         self.module_manager.start_modules()
+        self.control_module.start()
 
     def render(self, mode='human'):
         self.module_manager.render(self.world_module.display)

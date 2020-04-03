fe3effb3262e62c23cf40a228283a79d8ce7d5b4
MajidMoghadam2006 <majid.moghadam2006@gmail.com>
2020-04-03 12:10:33 -0700

    nautilus pod container creator

---
+++ archive/shtest.py
@@ -0,0 +1,35 @@
+from subprocess import Popen
+import os.path as osp
+import os
+import inspect
+import sys
+import subprocess
+import time
+
+from pathlib import Path
+#
+# currentPath = osp.dirname(osp.abspath(inspect.getfile(inspect.currentframe())))
+# parrentPath = Path(currentPath).parent
+# print(currentPath)
+# print(parrentPath)
+# carlaPath = '.' + str(parrentPath) + '/CarlaUE4.sh'
+# print(carlaPath)
+# Process = Popen(carlaPath, shell=False)
+
+
+import os
+
+source = os.path.dirname(__file__)
+parent = os.path.join(source, '../')
+command = os.path.join(parent, 'CarlaUE4.sh ' + 'Town04 -quality-level=low -windowed -world-port=3000  '
+                                                                  '-benchmark -fps=20 -opengl -carla-settings=CarlaSettings.ini')
+# DISPLAY= ./CarlaUE4.sh Town04 -quality-level=low -windowed -world-port=2000  -benchmark -fps=20 -opengl -carla-settings=CarlaSettings.ini
+# command = command.split()
+# command[0] = 'DISPLAY= ' + command[0]
+command = 'DISPLAY= ' + command
+print(command)
+process = Popen(command, shell=True, stdout=subprocess.PIPE)
+print(process.pid)
+time.sleep(10)
+# print('STDOUT:{}'.format(stdout))
+process.kill()

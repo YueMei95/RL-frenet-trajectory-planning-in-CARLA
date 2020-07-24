40373c23de03eb990eaabfb20b32d2650cd6ec07
Engin Tekin <engintekin@Engins-MacBook-Pro.local>
2020-07-24 11:16:22 -0700

    monitor plotting updated please read monitor_plot.py to see usage

--- tools/monitor_plot.py
+++ tools/monitor_plot.py
@@ -1,39 +1,68 @@
-import os.path as osp
-import os
-import inspect
-import sys
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 
-# currentPath = osp.dirname(osp.abspath(inspect.getfile(inspect.currentframe())))
-# sys.path.insert(1, currentPath + '/agents/stable_baselines/')
+'''
+Utility Folder in order to visualize Rewards
+Usage: place agent folder in logs/agent_xx
+args: 
+--agent_ids: agents to be plotted (default = None)
+--window_size: window size of moving (average default = 100)
+example usage:
+python reward_plotter.py --agent_ids 26 27 28 --window_size=10
+'''
 
-from stable_baselines.bench.monitor import load_results
 
-agent_id = 25
-n_smooth = 100
-col = 'r'       # 'r', 'l', 'max index'
 
-df = load_results('logs/agent_' + str(agent_id) + '/')
+def plot_rewards(args=None):
 
-# Calculate timestep for each episode using episodes length
-timestep = [df['l'][0]]
-for i, epslen in enumerate(df['l'][1:]):
-    timestep.append(epslen + timestep[i])
-df['timestep'] = timestep       # add timestep column to df
+    data = []
 
-print(df.head())
-data_n_step = []
-n_step = []
-for i in range(df.shape[0]-n_smooth):
-    data_n_step.append(df[col][i:i+n_smooth].mean())
-    n_step.append(df['timestep'][i+n_smooth-1])
+    for i in args.agent_ids:
+        data.append(pd.read_csv('logs/agent_{}/monitor.csv'.format(i),skiprows=1))
 
-# Plot training & validation loss values
-plt.plot(n_step, data_n_step)
-plt.title('episodic ' + col)
-plt.ylabel(col)
-plt.xlabel('timestep')
-# plt.legend(['Train', 'Test'], loc='upper left')
-plt.grid(True)
-# plt.xticks(np.arange(1, 100, 5))
-plt.show()
+    average = []
+    step_cum = []
+
+    for x in range(len(data)):
+        temp = []
+        temp_step = []
+        sum_ = 0
+        
+        for i in range(args.window_size-1):
+            sum_ += data[x]['l'][i] 
+        
+        for i in range(args.window_size-1,data[x]['r'].shape[0]):
+            temp.append(np.mean(data[x]['r'][i-args.window_size-1:i]))
+            sum_ += data[x]['l'][i]
+            temp_step.append(sum_)
+        
+        average.append(temp)
+        step_cum.append(temp_step)
+        
+
+    plt.figure(figsize=(12,8))
+
+    #lr = ['{}'.format(x) for x in range(start_id,end_id)]
+    lr = args.agent_ids
+    #colors = [np.random.rand(3,) for x in range(start_id,end_id)]
+    colors = [np.random.rand(3,) for x in args.agent_ids]
+    for i in range(len(lr)):
+        plt.plot(step_cum[i], average[i], '-', color=colors[i])
+
+        plt.title('CARLA')
+    plt.xlabel('TimeSteps')
+    plt.ylabel('Mean_Reward-{}'.format(args.window_size))
+    plt.legend(lr)
+    plt.grid()
+    plt.show()
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--agent_ids', nargs='+',type=int, default=None)
+    parser.add_argument('--window_size', type=int, default=100)
+    plot_rewards(args = parser.parse_args())
+
+
